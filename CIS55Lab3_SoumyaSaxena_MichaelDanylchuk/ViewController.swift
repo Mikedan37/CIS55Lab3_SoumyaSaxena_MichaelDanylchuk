d5a108f7f893fdf83030c17ffecca68866a4dd15
Michael Danylchuk <M_Danylchuk@Michaels-MacBook.local>
2017-02-08 21:23:06 -0800

    Initial Commit

---
+++ CIS55Lab3_SoumyaSaxena_MichaelDanylchuk/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  CIS55Lab3_SoumyaSaxena_MichaelDanylchuk
+//
+//  Created by Michael Danylchuk on 2/8/17.
+//
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
